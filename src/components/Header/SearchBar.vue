1e535db952d42f7b874c4f309391675ae5bd5c1b
Muhammed Abdrahman <muhammed03amin.com>
2022-10-26 13:26:16 +0600

    feat: add search logic

--- src/components/Header/SearchBar.vue
+++ src/components/Header/SearchBar.vue
@@ -1,18 +1,35 @@
 <template>
   <StyledSearchBarForm>
-    <StyledSearchInput />
+    <StyledSearchInput v-model="searchTerm" @change="changeSearchTerm" />
   </StyledSearchBarForm>
 </template>
 
 
 <script>
 import { StyledSearchBarForm, StyledSearchInput } from "@/components/Header/styled";
+import {mapGetters} from "vuex";
 
 export default {
   name: "SearchBar",
   components: {
     StyledSearchBarForm,
     StyledSearchInput
+  },
+  data() {
+    return {
+      searchTerm: ''
+    }
+  },
+  methods: {
+    changeSearchTerm() {
+      const prevParams = mapGetters(['getGamesParams']);
+      const updatedParams = {
+        prevParams,
+        search: this.searchTerm
+      }
+      this.$store.dispatch('mutateGamesParams', updatedParams);
+      this.$store.dispatch('fetchGames', updatedParams);
+    },
   }
 }
 </script>
